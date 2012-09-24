81c9a43e16b134a171c7dfbd115579873207d386
u_psm <lilingshui@126.com>
2012-09-24 17:32:39 +0800

    add the usage page of git add

---
+++ Code Storage/小知识点/Windows/git add使用_files/ad-blog.js
@@ -0,0 +1,6 @@
+﻿var ad_type = 'ibm27';
+var ad_sc = '<script type="text/javascript" '
+    + 'src="http://ad.csdn.net/scripts/ad-' + ad_type + '.js"'
+    + '></'
+    + 'script>';
+document.writeln(ad_sc);
